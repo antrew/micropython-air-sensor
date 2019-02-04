970fdcef3226e8be718a420c1181b8261b0946f1
Andrey Vetlugin <antrew@gmail.com>
2019-02-04 23:23:23 +0100

    Add pin mapping for WeMos D1 mini

---
+++ wemos.py
@@ -0,0 +1,16 @@
+# pin mapping for WeMos D1 mini
+
+D0 = 16  # GPIO
+D1 = 5  # GPIO, I2C SCL
+D2 = 4  # GPIO, I2C SDA
+D3 = 0  # GPIO
+D4 = 2  # GPIO
+D5 = 14  # GPIO, SPI SCK (Serial Clock)
+D6 = 12  # GPIO, SPI MISO (Master in, Slave out)
+D7 = 13  # GPIO, SPI MOSI (Master out, Slave in)
+D8 = 15  # GPIO, SPI SS (Slave select
+
+# A0 = A0 # Analog in, via ADC
+
+RX = 3  # Receive
+TX = 1  # Transmit
