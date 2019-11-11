009ad5a7375745a0d2df904d01d0825c629f1218
Andrey Vetlugin <antrew@gmail.com>
2019-11-11 15:05:16 +0100

    WIP document special boot pins

--- wemos.py
+++ wemos.py
@@ -3,12 +3,12 @@
 D0 = 16  # GPIO, wake up
 D1 = 5  # GPIO, I2C SCL
 D2 = 4  # GPIO, I2C SDA
-D3 = 0  # GPIO
-D4 = 2  # GPIO
+D3 = 0  # * GPIO
+D4 = 2  # * GPIO
 D5 = 14  # GPIO, SPI SCK (Serial Clock)
 D6 = 12  # GPIO, SPI MISO (Master in, Slave out)
 D7 = 13  # GPIO, SPI MOSI (Master out, Slave in)
-D8 = 15  # GPIO, SPI SS (Slave select
+D8 = 15  # * GPIO, SPI SS (Slave select)
 
 # A0 = A0 # Analog in, via ADC
 A0 = 0
@@ -19,3 +19,7 @@ TX = 1  # Transmit
 # used for flashing:
 #GPIO0, GPIO2 and GPIO15
 #  D3     D4         D8
+
+# D3 GPIO0
+# D4 GPIO2
+# D8 GPIO15
